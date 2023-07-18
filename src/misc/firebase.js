019abd033825a7603597b42397f9458960db4188
Anshuma Gahlot <anshumavgahlot10@gmail.com>
2023-07-18 13:34:18 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'; 
+
+
+const config={
+    apiKey: "AIzaSyBgHy7Im9VbCIvgh3IpY_L_ATgZYKtQ5SU",
+    authDomain: "chat-web-app-d96ed.firebaseapp.com",
+    projectId: "chat-web-app-d96ed",
+    storageBucket: "chat-web-app-d96ed.appspot.com",
+    messagingSenderId: "699841897424",
+    appId: "1:699841897424:web:ed7c79514e6818eed90fd5"
+  };
+  const app=firebase.initializeApp(config);
+  
